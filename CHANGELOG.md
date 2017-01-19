f105a8dc26f83a9239efd56f598e8d0fd8c813f6
offirgolan <offirgolan@gmail.com>
2017-01-18 22:22:32 -0800

    Install ember-cli-changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,5 @@
-# Changelog
+Changelog
+=========
 
 ## v0.1.2
 
--- config/release.js
+++ config/release.js
@@ -1,24 +1,19 @@
 /* jshint node:true */
-var generateChangelog = require('ember-addon-genie/lib/tasks/generate-changelog');
 // var RSVP = require('rsvp');
+var generateChangelog = require('ember-cli-changelog/lib/tasks/release-with-changelog');
 
 // For details on each option run `ember help release`
 module.exports = {
-  publish: true,
-  beforeCommit: generateChangelog
   // local: true,
   // remote: 'some_remote',
   // annotation: "Release %@",
   // message: "Bumped version to %@",
   // manifest: [ 'package.json', 'bower.json', 'someconfig.json' ],
-  // publish: true,
+  publish: true,
   // strategy: 'date',
   // format: 'YYYY-MM-DD',
   // timezone: 'America/Los_Angeles',
-  //
-  // beforeCommit: function(project, versions) {
-  //   return new RSVP.Promise(function(resolve, reject) {
-  //     // Do custom things here...
-  //   });
-  // }
+
+  beforeCommit: generateChangelog
+
 };
