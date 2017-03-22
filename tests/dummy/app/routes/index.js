e537ebc1519163704bf6f493dedc89cd3543d87f
Offir Golan <offirgolan@gmail.com>
2017-03-22 10:44:37 -0700

    Use rootURL to determine redirect

--- tests/dummy/app/routes/index.js
+++ tests/dummy/app/routes/index.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
+import ENV from '../config/environment';
 
 export default Ember.Route.extend({
   redirect() {
-    window.location.replace('docs');
+    window.location.replace(`${ENV.rootURL}docs`);
   }
 });
