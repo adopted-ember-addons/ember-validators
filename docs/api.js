14abddeb7cff2917e51ce929c2c4f4a267d258d9
Offir Golan <offirgolan@gmail.com>
2017-03-22 10:32:01 -0700

    Initial gh-pages release

---
+++ docs/api.js
@@ -0,0 +1,27 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Collection",
+        "Confirmation",
+        "DS Error",
+        "Date",
+        "Exclusion",
+        "Format",
+        "Inclusion",
+        "Length",
+        "Messages",
+        "Number",
+        "Presence"
+    ],
+    "modules": [
+        "Validators"
+    ],
+    "allModules": [
+        {
+            "displayName": "Validators",
+            "name": "Validators"
+        }
+    ],
+    "elements": []
+} };
+});
