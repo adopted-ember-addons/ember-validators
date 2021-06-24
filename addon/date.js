059808225056e9e744383059b8ddb17f52f09483
scott-newcomer <snewcomer24@gmail.com>
2021-06-24 09:50:05 -0500

    [Major]: Rm Moment and Node 10 removal

--- addon/date.js
+++ addon/date.js
@@ -1,7 +1,6 @@
-import { isEmpty, isNone } from '@ember/utils';
+import { isEmpty } from '@ember/utils';
 import { set } from '@ember/object';
 import validationError from 'ember-validators/utils/validation-error';
-import moment from 'moment';
 
 /**
  * @class Date
@@ -25,12 +24,8 @@ import moment from 'moment';
  * @param {String} attribute
  */
 export default function validateDate(value, options) {
-  if (!moment) {
-    throw new Error('MomentJS is required to use the Date validator.');
-  }
-
   let errorFormat = options.errorFormat || 'MMM Do, YYYY';
-  let { format, precision, allowBlank } = options;
+  let { locale, format, precision, allowBlank } = options;
   let { before, onOrBefore, after, onOrAfter } = options;
   let date;
 
@@ -39,54 +34,54 @@ export default function validateDate(value, options) {
   }
 
   if (format) {
-    date = parseDate(value, format, true);
+    date = parseDate(value, format, locale);
 
     // Check to see if the passed date is actually a valid date.
     // This can be done by disabling the strict parsing
-    const isActualDate = parseDate(value, format).isValid();
+    const isActualDate = isValidDate(parseDate(value, format, locale));
 
     if (!isActualDate) {
       return validationError('date', value, options);
-    } else if (!date.isValid()) {
+    } else if (!isValidDate(date)) {
       return validationError('wrongDateFormat', value, options);
     }
   } else {
-    date = parseDate(value);
+    date = parseDate(value, null, locale);
 
-    if (!date.isValid()) {
+    if (!isValidDate(date.isValid())) {
       return validationError('date', value, options);
     }
   }
 
   if (before) {
-    before = parseDate(before, format);
+    before = parseDate(before, format, locale);
 
     if (!date.isBefore(before, precision)) {
-      set(options, 'before', before.format(errorFormat));
+      set(options, 'before', format(before, errorFormat));
       return validationError('before', value, options);
     }
   }
 
   if (onOrBefore) {
-    onOrBefore = parseDate(onOrBefore, format);
+    onOrBefore = parseDate(onOrBefore, format, locale);
 
     if (!date.isSameOrBefore(onOrBefore, precision)) {
-      set(options, 'onOrBefore', onOrBefore.format(errorFormat));
+      set(options, 'onOrBefore', format(onOrBefore, errorFormat));
       return validationError('onOrBefore', value, options);
     }
   }
 
   if (after) {
-    after = parseDate(after, format);
+    after = parseDate(after, format, locale);
 
     if (!date.isAfter(after, precision)) {
-      set(options, 'after', after.format(errorFormat));
+      set(options, 'after', format(after, errorFormat));
       return validationError('after', value, options);
     }
   }
 
   if (onOrAfter) {
-    onOrAfter = parseDate(onOrAfter, format);
+    onOrAfter = parseDate(onOrAfter, format, locale);
 
     if (!date.isSameOrAfter(onOrAfter, precision)) {
       set(options, 'onOrAfter', onOrAfter.format(errorFormat));
@@ -97,12 +92,30 @@ export default function validateDate(value, options) {
   return true;
 }
 
-export function parseDate(date, format, useStrict = false) {
-  if (date === 'now' || isEmpty(date)) {
-    return moment();
+export function parseDate(date, format, locale) {
+  if (date === 'now' || date === null || date === undefined) {
+    return new Date();
   }
 
-  return isNone(format)
-    ? moment(new Date(date))
-    : moment(date, format, useStrict);
+  return format ? new Date(date) : new Intl.DateTimeFormat(locale, format);
+}
+
+function isValidDate(d) {
+  return d instanceof Date && !isNaN(d);
+}
+
+function isBefore(date, precision) {
+
+}
+
+function isSameOrBefore(date, precision) {
+
+}
+
+function isAfter(date, precision) {
+
+}
+
+function isSameOrAfter(date, precision) {
+
 }
