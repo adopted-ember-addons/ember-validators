db23766ebbfd77fe501968ab47540f225c2938bb
Sergey Astapov <sergeastapov@gmail.com>
2025-03-28 17:24:37 -0400

    Convert to TypeScript

--- src/ds-error.ts
+++ src/ds-error.ts
@@ -1,5 +1,12 @@
-import validationError from './utils/validation-error.js';
 import { get } from '@ember/object';
+import validationError, {
+  type IValidationError,
+} from './utils/validation-error.ts';
+
+interface IOptions {
+  path: string;
+  key: boolean;
+}
 
 /**
  *  @class DS Error
@@ -13,27 +20,42 @@ import { get } from '@ember/object';
  * @param {Object} model
  * @param {String} attribute
  */
-export default function validateDsError(value, options, model, attribute) {
-  let { path, key } = getPathAndKey(attribute);
+export default function validateDsError(
+  value: unknown,
+  options: IOptions,
+  model: object,
+  attribute: string,
+): true | IValidationError<unknown, IOptions> {
+  const { path, key } = getPathAndKey(attribute);
 
-  let errors = get(model, path);
+  const errors = get(model, path) as
+    | undefined
+    | {
+        has: (key: string) => unknown;
+        errorsFor: (
+          key: string,
+        ) => Array<{ attribute: string; message: string }>;
+      };
 
   if (errors && errors.has && errors.has(key)) {
-    let errorsFor = errors.errorsFor(key);
+    const errorsFor = errors.errorsFor(key);
     return validationError(
       'ds',
       null,
       options,
-      errorsFor.length ? errorsFor[errorsFor.length - 1].message : [],
+      errorsFor.length ? errorsFor[errorsFor.length - 1]!.message : [],
     );
   }
 
   return true;
 }
 
-export function getPathAndKey(attribute) {
-  let path = attribute.split('.');
-  let key = path.pop();
+export function getPathAndKey(attribute: string): {
+  path: string;
+  key: string;
+} {
+  const path = attribute.split('.');
+  const key = path.pop()!;
 
   path.push('errors');
 
--- src/utils/validation-error.ts
+++ src/utils/validation-error.ts
@@ -1,3 +1,53 @@
-export default function validationError(type, value, context, message) {
+export default function validationError<V, C>(
+  type: IValidationError<V, C>['type'],
+  value: V,
+  context: C,
+  message?: string | string[],
+): IValidationError<V, C> {
   return { type, value, context, message };
+}
+
+export interface IValidationError<V, C> {
+  type:
+    | 'collection'
+    | 'confirmation'
+    | 'date'
+    | 'ds'
+    | 'ds-error'
+    | 'exclusion'
+    | 'format'
+    | 'inclusion'
+    | 'length'
+    | 'messages'
+    | 'number'
+    | 'presence'
+    | 'singular'
+    | 'email'
+    | 'phone'
+    | 'url'
+    | 'invalid'
+    | 'wrongLength'
+    | 'between'
+    | 'tooShort'
+    | 'tooLong'
+    | 'notANumber'
+    | 'notAnInteger'
+    | 'equalTo'
+    | 'lessThan'
+    | 'lessThanOrEqualTo'
+    | 'greaterThan'
+    | 'greaterThanOrEqualTo'
+    | 'positive'
+    | 'odd'
+    | 'even'
+    | 'multipleOf'
+    | 'blank'
+    | 'present'
+    | 'before'
+    | 'onOrBefore'
+    | 'after'
+    | 'onOrAfter';
+  value: V;
+  context: C;
+  message?: string | string[];
 }
